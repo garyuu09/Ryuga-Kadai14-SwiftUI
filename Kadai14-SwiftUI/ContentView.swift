5fc282c7f6e26421d8af91dfbf132f6015775887
Ryuga <ribereo.minami@gmail.com>
2023-04-06 21:38:44 +0900

    フルーツのリストを実装

--- Kadai14-SwiftUI/ContentView.swift
+++ Kadai14-SwiftUI/ContentView.swift
@@ -7,17 +7,43 @@
 
 import SwiftUI
 
+struct Fruit {
+    var code = UUID()
+    var name: String
+    var isChecked: Bool
+}
+
 struct ContentView: View {
+    var fruits = [
+        Fruit(name: "りんご", isChecked: false),
+        Fruit(name: "みかん", isChecked: true),
+        Fruit(name: "バナナ", isChecked: false),
+        Fruit(name: "パイナップル", isChecked: true)
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List {
+                ForEach(fruits, id: \.code) { fruit in
+                    ZStack {
+                        HStack{
+                            Image(systemName: fruit.isChecked ? "checkmark" : "")
+                                .foregroundColor(Color.orange)
+                            Spacer()
+                        }
+                        HStack {
+                            Spacer().frame(width: 30)
+                            Text(fruit.name)
+                            Spacer()
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .listStyle(.plain)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
