b995ec7a9bdb6e5b8bf887f401f0a52fc4285b3a
akio0911 <akio0911@gmail.com>
2023-04-08 15:39:45 +0900

    feedback

--- Kadai14-SwiftUI/AddItemView.swift
+++ Kadai14-SwiftUI/AddItemView.swift
@@ -8,13 +8,10 @@
 import SwiftUI
 
 struct AddItemView: View {
-    @Environment(\.dismiss) var dismiss
     @State private var newItem = ""
-    @Binding var fruits: [Fruit]
-    //    @Binding var fruits: Array<Dictionary<String, Bool>>
-    //    @Binding var fruits: Binding<[Fruit]>
-    //    @Binding var fruits: Fruit
-    //    色々試して、なんとか正解?にたどり着いたが少し、理解が甘い箇所のため要復習
+
+    let didSave: (Fruit) -> Void
+    let didCancel: () -> Void
 
     var body: some View {
         NavigationStack{
@@ -31,18 +28,14 @@ struct AddItemView: View {
             .toolbar {
                 ToolbarItem(placement: .cancellationAction) {
                     Button("Cancel") {
-                        dismiss()
+                        didCancel()
                     }
                 }
             }
             .toolbar {
                 ToolbarItem(placement: .confirmationAction) {
                     Button("Save") {
-                        if !newItem.isEmpty {
-                            fruits.append(Fruit(name: newItem, isChecked: false))
-                            newItem = ""
-                        }
-                        dismiss()
+                        didSave(Fruit(name: newItem, isChecked: false))
                     }
                 }
             }
@@ -59,9 +52,8 @@ struct CustomTextFieldStyle: ViewModifier {
 }
 
 struct Preview: View {
-    @State var fruits = [Fruit(name: "りんご", isChecked: false)]
     var body: some View {
-        AddItemView(fruits: $fruits)
+        AddItemView(didSave: { _ in }, didCancel: {})
     }
 }
 
